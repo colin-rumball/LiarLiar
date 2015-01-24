5674dca48dffa6e05c625e19c9654eb01d325947
stevengans <stevenbgans@gmail.com>
2015-01-24 18:54:09 -0500

    Steven's commits
    Suck it C#

---
+++ Assets/Scripts/DriverUserControl.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+	
+	public class DriverUserControl : MonoBehaviour {
+		
+		float speed = 4.0f;
+		
+		void Update() {
+		if (tag == "Player") {
+			var move = new Vector3(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"), 0);
+			transform.position += move * speed * Time.deltaTime;
+		}
+		if (tag == "Player2") {
+			var move = new Vector3(Input.GetAxis("Horizontal2"), Input.GetAxis("Vertical2"), 0);
+			transform.position += move * speed * Time.deltaTime;
+		}
+		}
+	}
+
---
+++ Assets/Scripts/Platformer2DUserControl.cs
@@ -0,0 +1,46 @@
+﻿using UnityEngine;
+using UnitySampleAssets.CrossPlatformInput;
+
+namespace UnitySampleAssets._2D
+{
+
+    [RequireComponent(typeof (PlatformerCharacter2D))]
+    public class Platformer2DUserControl : MonoBehaviour
+    {
+        private PlatformerCharacter2D character;
+		private bool jump;
+
+        private void Awake()
+        {
+            character = GetComponent<PlatformerCharacter2D>();
+        }
+
+        private void Update()
+        {
+            if(!jump)
+            // Read the jump input in Update so button presses aren't missed.
+            if (tag == "Player") {
+				jump = CrossPlatformInputManager.GetButtonDown("Jump");
+			}
+			if (tag == "Player2") {
+				jump = CrossPlatformInputManager.GetButtonDown("Jump2");
+			}
+        }
+
+        private void FixedUpdate()
+        {
+			if (tag == "Player") {
+            float h = CrossPlatformInputManager.GetAxis("Horizontal");
+            // Pass all parameters to the character control script.
+            character.Move(h, false, jump);
+            jump = false;
+			}
+			if (tag == "Player2") {
+				float h = CrossPlatformInputManager.GetAxis("Horizontal2");
+				// Pass all parameters to the character control script.
+				character.Move(h, false, jump);
+				jump = false;
+			}
+        }
+    }
+}
