ba2e89dd125e9ada9655e0719aff0ec44a229c9a
colin-rumball <colin.rumball@gmail.com>
2015-01-24 11:18:57 -0500

    Added Basic FPS and QTE

---
+++ Assets/Scripts/FPS/FPS_Gun.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FPS_Gun : MonoBehaviour {
+
+	public const int SPEED = 5;
+
+	// Use this for initialization
+	void Start () {
+		this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y, this.transform.position.z-0.5f);
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKeyDown(KeyCode.Tab))
+		{
+			Debug.DrawRay(transform.position, transform.forward, Color.green);
+			RaycastHit hitInfo = new RaycastHit();
+			bool hit = Physics.Raycast(transform.position, transform.forward, out hitInfo);
+			if (hit) 
+			{
+				Debug.Log("Hit " + hitInfo.transform.gameObject.name);
+				if (hitInfo.transform.gameObject.tag == "FPS-Shootable")
+				{
+					Debug.Log ("It's working!");
+				} else {
+					Debug.Log ("nopz");
+				}
+			} else {
+				Debug.Log("No hit");
+			}
+		}
+
+		if (Input.GetKey(KeyCode.A))
+		{
+			this.transform.position = new Vector3(this.transform.position.x- SPEED*Time.deltaTime, this.transform.position.y, this.transform.position.z);
+		} else if (Input.GetKey(KeyCode.D))
+		           {
+			this.transform.position = new Vector3(this.transform.position.x+ SPEED*Time.deltaTime, this.transform.position.y, this.transform.position.z);
+		} else if (Input.GetKey(KeyCode.S))
+		           {
+			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y - SPEED*Time.deltaTime, this.transform.position.z);
+		} else if (Input.GetKey(KeyCode.W))
+		{
+			this.transform.position = new Vector3(this.transform.position.x, this.transform.position.y + SPEED*Time.deltaTime, this.transform.position.z);
+		}
+	}
+}
---
+++ Assets/Scripts/QTE/QTE_Input.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class QTE_Input : MonoBehaviour 
+{
+
+	public KeyCode[] expectedInput;
+	private int currentIndex = 0;
+
+	// Use this for initialization
+	void Start () 
+	{
+		fillArray("ADDFGT");
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		print(expectedInput[currentIndex]);
+		if (Input.GetKeyDown(expectedInput[currentIndex]))
+		{
+			if (currentIndex < expectedInput.Length-1)
+				currentIndex++;
+		}
+	}
+
+	void fillArray(string str)
+	{
+		char[] charArr = str.ToCharArray();
+		expectedInput = new KeyCode[charArr.Length];
+		for (int i = 0; i < charArr.Length; i++)
+		{
+			expectedInput[i] = (KeyCode)System.Enum.Parse(typeof(KeyCode), charArr[i].ToString());
+		}
+	}
+}
