63938c7992d41477ec9464e71fbb2862e4792dec
colin-rumball <colin.rumball@gmail.com>
2015-01-25 19:42:40 -0500

    worked on animations

--- Assets/Scripts/Introduction/Introduction.cs
+++ Assets/Scripts/Introduction/Introduction.cs
@@ -4,24 +4,14 @@ using System.Collections;
 
 public class Introduction : MonoBehaviour 
 {
-	public Text line1, line2;
-	float timer = 5.0f;
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
+	public bool GotoInterrogation = false;
 	// Update is called once per frame
 	void Update () {
-		timer -= Time.deltaTime;
-		if (timer <= 2.0f && line1.text == "")
-		{
-			line1.text = "Hey!";
-		} else if (timer <= 0.0f)
-		{
-			line2.text = "Stop!";
-		}
-		
+
+		if (GotoInterrogation) {
+						Application.LoadLevel ("Interrogation");
+				}
 		
 	}
+
 }
